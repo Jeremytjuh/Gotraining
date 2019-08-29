d7b8f68f28136d0f2c3c0ef04dd712ba94a39262
Jeremytjuh <jeremynelemans@hotmail.com>
2019-08-29 12:46:16 +0200

    Added main.go

---
+++ TryingOut/mypack/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/Jeremytjuh/Gotraining/TryingOut/mypack/mypack"
+)
+
+func main() {
+	xi := []int{1, 2, 3, 4, 5}
+	xii := []int{6, 7, 8, 9, 10}
+	mypack.AddIntSlice(xi, xii)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Jeremytjuh/Gotraining
+
+go 1.12
