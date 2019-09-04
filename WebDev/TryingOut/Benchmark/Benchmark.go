854732a7221bc82816acfd276bbb455744edd29d
Jeremytjuh <jeremynelemans@hotmail.com>
2019-09-04 14:13:01 +0200

    CBA

---
+++ WebDev/TryingOut/Benchmark/Benchmark.go
@@ -0,0 +1,13 @@
+package Benchmark
+
+import (
+	"strings"
+)
+
+func CallSplit(s, sep string) []string {
+	return strings.Split(s, sep)
+}
+
+func CallFields(s string) []string {
+	return strings.Fields(s)
+}
